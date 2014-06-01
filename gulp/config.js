f1bc83afd53a1b2b32358d7003d68c1109c833bf
Mark Goodyear <hello@markgoodyear.com>
2014-06-01 22:37:41 +0100

    Use `public` folder instead

--- gulp/config.js
+++ gulp/config.js
@@ -3,6 +3,6 @@
  */
 global.config = {
   src:  'src',      // Source path
-  dist: 'build',    // Build path
+  dist: 'public',    // Build path
   banner: '/*! Big Bite Creative | bigbitecreative.com | Copyright (c) 2014 */\n'
 };
